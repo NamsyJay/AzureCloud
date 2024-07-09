eace0147a98eb4d38b7e32e6bd47281e0e866665
Onamusi Babajide <50235388+NamsyJay@users.noreply.github.com>
2024-07-09 15:17:55 +0100

    Update main.tf

--- Azure IaC/Web-App/main.tf
+++ Azure IaC/Web-App/main.tf
@@ -1,3 +1,12 @@
+terraform {
+    required_providers {
+        azurerm ={
+            source = "hashicorp/azurerm"
+            version = "=3.66.0"
+        }
+    }
+}
+
 provider "azurerm" {
     features {}
 }
@@ -10,7 +19,7 @@ resource "azurerm_resource_group" "resource_group" {
 
 # create a storage account
 resource "azurerm_storage_account" "storage_account" {
-    name                     = "terraform-azure-NHS"
+    name                     = "terraformazurenhs"
     resource_group_name      = azurerm_resource_group.resource_group.name
     location                 = azurerm_resource_group.resource_group.location
     account_tier             = "Standard"
@@ -18,14 +27,15 @@ resource "azurerm_storage_account" "storage_account" {
     account_kind             = "StorageV2"
 
     static_website {
-    index_document = "index.html"
+        index_document = "index.html"
     }
 }
+
 
 # Add index.html File
 resource "azurerm_storage_blob" "blob" {
     name                   = "index.html"
-    storage_account_name   = azurerm_storage_account.strorage_account.name
+    storage_account_name   = azurerm_storage_account.storage_account.name
     storage_container_name = "$web"
     type                   = "Block"
     content_type           = "text/html"
