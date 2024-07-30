d91329981096e7c641ca965291a03fbc5ba5ecf2
Onamusi Babajide <50235388+NamsyJay@users.noreply.github.com>
2024-07-30 13:28:16 +0100

    Create varaibles.tf

---
+++ Azure IaC/AzureStorage/ResourceGroup/varaibles.tf
@@ -0,0 +1,9 @@
+variable "base_name" {
+    type        = string
+    description = "the base name of the resource group and storage account"
+}
+
+variable "location" {
+    type        = string
+    description = "the location for the deployment"
+}
