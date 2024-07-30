b8b29885768374811676d4dd663226edf68a3769
Onamusi Babajide <50235388+NamsyJay@users.noreply.github.com>
2024-07-30 13:31:08 +0100

    Create variable.tf

---
+++ Azure IaC/AzureStorage/StorageAccount/variable.tf
@@ -0,0 +1,14 @@
+variable "base_name" {
+    type = string
+    description = "the strorage account base name"
+}
+
+variable "resource_group_name" {
+    type = string
+    description = "Name of The resource group"
+}
+
+variable "location" {
+    type = string
+    description = "The location for the deployment"
+}
