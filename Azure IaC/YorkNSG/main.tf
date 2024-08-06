0438fe2f4284ecdd0291d9d8f9c2692c88797477
Onamusi Babajide <50235388+NamsyJay@users.noreply.github.com>
2024-08-06 17:50:38 +0100

    Update main.tf

--- Azure IaC/YorkNSG/main.tf
+++ Azure IaC/YorkNSG/main.tf
@@ -1,40 +1,44 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source = "tfproviders/azurerm"
-            version = "3.86.0"
-        }
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.114.0"
     }
+  }
 }
+
 provider "azurerm" {
     features {
-    
     }
 }
 
-resource "azurerm" "resource_group"{
+resource "azurerm_resource_group" "resource_group"{
     name                = var.resource_group_name
     location            = var.location
     tags                = var.tags
 }
 
-resource "azure_netwwork_security_group" "network_security_group" {
+resource "azurerm_network_security_group" "network_security_group" {
     name                = var.name
     location            = var.location
-    resource_group_name = azure_netwwork_security_group.name
+    resource_group_name = azurerm_resource_group.resource_group.name
 
     dynamic "security_rule" {
-        for_each = var.nsg.rules
-        content {
-            name                        = security_rule.value["name"]
-            priority                    = security_rule.value["priority"]
-            direction                   = security_rule.value["direction"]
-            access                      = security_rule.value["access"]
-            protocol                    = security_rule.value["protocol"]
-            source_port_range           = security_rule.value["source_port_range"]
-            destination_port_range      = security_rule.value["destination_port_range"]
-            source_address_prefix       = security_rule.value["source_address_prefix"]
-            destination_address_prefix  = security_rule.value["destination_address_prefix"]
-        }
+      for_each = var.nsg_rules
+      content {
+      name                       = security_rule.value["name"]
+      priority                   = security_rule.value["priority"]
+      direction                  = security_rule.value["direction"]
+      access                     = security_rule.value["access"]
+      protocol                   = security_rule.value["protocol"]
+      source_port_range          = security_rule.value["source_port_range"]
+      destination_port_range     = security_rule.value["destination_port_range"]
+      source_address_prefix      = security_rule.value["source_address_prefix"]
+      destination_address_prefix = security_rule.value["destination_address_prefix"]
     }
+  }
+
+    tags = var.tags
 }
+
+
