580bad5edca1615c7533bcbfbd75a1bf58a1f4db
Onamusi Babajide <50235388+NamsyJay@users.noreply.github.com>
2024-08-06 14:57:48 +0100

    Create main.tf

---
+++ Azure IaC/YorkNSG/main.tf
@@ -0,0 +1,14 @@
+terraform {
+    required_providers {
+        azurerm = {
+            source = "tfproviders/azurerm"
+            version = "3.86.0"
+        }
+    }
+}
+
+provider "azurerm" {
+    features {
+    
+    }
+}
