951f653611caddd80853e28ae49cd4daeb17004d
Onamusi Babajide <50235388+NamsyJay@users.noreply.github.com>
2024-08-06 15:47:06 +0100

    Update main.tf

--- Azure IaC/YorkNSG/main.tf
+++ Azure IaC/YorkNSG/main.tf
@@ -12,3 +12,30 @@ provider "azurerm" {
     
     }
 }
+
+resource "azurerm" "resource_group"{
+    name                = var.resource_group_name
+    location            = var.location
+    tags                = var.tags
+}
+
+resource "azure_netwwork_security_group" "network_security_group" {
+    name                = var.name
+    location            = var.location
+    resource_group_name = azure_netwwork_security_group.name
+
+    dynamic "security_rule" {
+        for_each = var.nsg.rules
+        content {
+            name                        = security_rule.value["name"]
+            priority                    = security_rule.value["priority"]
+            direction                   = security_rule.value["direction"]
+            access                      = security_rule.value["access"]
+            protocol                    = security_rule.value["protocol"]
+            source_port_range           = security_rule.value["source_port_range"]
+            destination_port_range      = security_rule.value["destination_port_range"]
+            source_address_prefix       = security_rule.value["source_address_prefix"]
+            destination_address_prefix  = security_rule.value["destination_address_prefix"]
+        }
+    }
+}
